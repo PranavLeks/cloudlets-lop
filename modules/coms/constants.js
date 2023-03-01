8bebf78e109630b011371e7d1163c6643403995d
Mayank Mathur <Mynk-9@users.noreply.github.com>
2023-03-01 22:17:56 +0530

    Added comms module

---
+++ modules/coms/constants.js
@@ -0,0 +1,10 @@
+export const STORAGE_MSG = 'storage_update';
+export const CPU_MSG = 'cpu_update';
+export const TRANSFER_DATA_MSG = 'transfer_data';
+export const TRANSFER_TASK_MSG = 'transfer_task';
+
+export const SENDER_PARAM = 'sender';
+export const STATE_PARAM = 'state';
+export const DATA_PARAM = 'data';
+export const DATA_FORMAT_PARAM = 'format';
+export const TASK_PARAM = 'task';
